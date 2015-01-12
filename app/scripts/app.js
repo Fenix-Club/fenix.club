de43c28c74dfee7a1b87ad951d72ee0dd8dbae46
Kacper Kula <kulak@kulak-5.local>
2015-01-12 21:59:29 +0100

    Hello World on the homepage.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,28 @@
+'use strict';
+
 angular.module('fenix', [
+
+  // External modules here
+  'ui.router',
+  'ui.router.stateHelper',
+  'ui.bootstrap',
+
+  // Project modules here
   'fenix.templates'
-]);
+])
+
+.config(function($stateProvider, stateHelperProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.when('', '/');
+
+  stateHelperProvider.setNestedState({
+    name: 'homepage',
+    url: '/',
+    views: {
+      root: {
+        templateUrl: 'templates/homepage.html'
+      }
+    }
+  }, true);
+
+});
