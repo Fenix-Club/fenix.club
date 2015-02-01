eaf6f066d1cd3618683d27d8bf1942f8b180dcdb
zakaprov <zakaprov@gmail.com>
2015-02-01 20:37:40 +0100

    New JSON format for events. Basic colous depending on the event type.

--- app/scripts/calendar/controllers.js
+++ app/scripts/calendar/controllers.js
@@ -5,15 +5,24 @@ angular.module('fenix.calendar')
 .controller('calendarController', function($scope, $http, eventService) {
 
   $scope.eventSource = [];
+  var colorPhysics = "#FF0000";
+  var colorTechnology = "#00FF00";
 
   eventService.async().then(function(data) {
-    $scope.eventSource.push(data);
+
+    var parsedData = _.map(data, function(currentEvent) {
+        currentEvent.color = (currentEvent.eventType === "physics" ? colorPhysics : colorTechnology);
+        return currentEvent;
+    });
+
+    $scope.eventSource.push(parsedData);
   });
 
 	$scope.uiConfig = {
       calendar:{
-        height: 450,
+        height: 800,
         editable: false,
+        lang: "pl",
         header:{
           left: 'month basicWeek basicDay agendaWeek agendaDay',
           center: 'title',
